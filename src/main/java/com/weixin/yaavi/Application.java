ea4660e031f48411087b6d51604d7a97f40681e8
gaoguoxiang <snowxuyu@gmail.com>
2016-12-01 15:35:06 +0800

    启动方式修改

--- src/main/java/com/weixin/yaavi/Application.java
+++ src/main/java/com/weixin/yaavi/Application.java
@@ -1,6 +1,8 @@
 package com.weixin.yaavi;
 
-import org.framework.basic.server.NettyServerBootstrap;
+
+import org.framework.basic.server.NettyApplication;
+import org.framework.basic.server.annotation.NettyBootstrap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,13 +10,17 @@ import org.framework.basic.server.NettyServerBootstrap;
  * Date: 2016/11/30
  * Time: 1:25
  */
+@NettyBootstrap(applicationContext="classpath:/springs/applicationContext.xml", servletContext="classpath:/springs/springmvc-servlet.xml")
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        NettyApplication.run(Application.class, args);
+
         // 创建->设置application父容器->设置springmvc子容器->设置server参数->启动
-        NettyServerBootstrap.createServer()
+       /* NettyServerBootstrap.createServer()
                 .setSpringApplicationContext("classpath:/springs/applicationContext.xml")
                 .setSpringServletContext("classpath:/springs/springmvc-servlet.xml")
                 .parseCommondArguments(args)
-                .start();
+                .start();*/
     }
 }
